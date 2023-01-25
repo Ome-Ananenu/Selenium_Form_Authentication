ee6b3b874e2b25474e78a31ad96b82b58205f2cf
OmeAnanenu <omenebele.ananenu@teamapt.com>
2023-01-25 22:02:09 +0100

    initial commit

---
+++ src/test/java/base/FormAuthenticationTests.java
@@ -0,0 +1,62 @@
+package base;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import pages.FormAuthenticationPage;
+import pages.LoggedInPage;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.testng.AssertJUnit.assertTrue;
+
+public class FormAuthenticationTests {
+
+    private ChromeDriver driver;
+    private FormAuthenticationPage formAuthenticationPage;
+
+
+    @BeforeEach
+    void setUp() {
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver");
+
+        driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/");
+        driver.manage().window().maximize();
+
+        formAuthenticationPage = new FormAuthenticationPage(driver);
+        formAuthenticationPage.navigateToFormAuthenticationPage();
+    }
+
+    @Test
+    public void testToVerifyOutcome_WhenUserLogsInWithValidPayload() throws InterruptedException {
+        formAuthenticationPage.fillLoginForm("tomsmith", "SuperSecretPassword!");
+        Thread.sleep(2000);
+
+        LoggedInPage loggedInPage = new LoggedInPage(driver);
+
+        String successMessage = "You logged into a secure area!";
+        assertTrue(loggedInPage.getAlertText().contains(successMessage));
+    }
+
+    @Test
+    public void testToVerifyOutcomeWhen_UserLogsInWithInValidPayload() throws InterruptedException {
+        formAuthenticationPage.fillLoginForm("thomas", "SecretPassword!");
+        Thread.sleep(2000);
+
+        String errorMsg = "Your username is invalid!";
+
+        assertTrue(formAuthenticationPage.getAlertText().contains(errorMsg));
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+    }
+}
